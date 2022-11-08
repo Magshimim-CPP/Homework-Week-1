271c92070dcc3320f3c47e463572f4074b238d48
Ohad Shaked <ohad.saked@gmail.com>
2022-11-08 16:47:11 +0200

    finished writing Utils code, and added comments to the header and code files.

--- HW_week1_OhadShaked/HW_week1_OhadShaked/Utils.cpp
+++ HW_week1_OhadShaked/HW_week1_OhadShaked/Utils.cpp
@@ -8,6 +8,7 @@ using namespace std;
 //setting constant define variables
 #define ZERO 0
 #define ONE 1
+#define ARR_LEN 10
 
 /*
 Function reverses the numbers stored in a given array using the stack structure.
@@ -17,21 +18,40 @@ Output: none.
 void reverse(int* nums, unsigned int size)
 {
 	Stack* s = new Stack();
-	int* tempArr = nums;
 
 	//looping to push all of the numbers from the array into the stack.
 	for (int i = ZERO; i < size; i++)
 	{
-		push(s, tempArr[i]);
+		push(s, nums[i]);
 	}
-	//popping the numbers from the stack (starts from last one that was enterd --> reversed).
+	//popping the numbers from the stack (starts from last one that was pushed --> reversed).
 	for (int i = ZERO; i < size; i++)
 	{
 		nums[i] = pop(s);
 	}
 }
 
+/*
+Function takes input for 10 numbers in a array, reverses the array, and returns the reversed array.
+Input: no input.
+Output: the created reversed array (int* nums).
+*/
 int* reverse10()
 {
-	return NULL;
+	//creating a dynamic array for the numbers.
+	int* nums = new int(ARR_LEN);
+
+	//taking input for 10 numbers.
+	cout << "Please enter 10 numbers:\n" << endl;
+	for (int i = ZERO; i < ARR_LEN; i++)
+	{
+		cout << "num 1: " << endl;
+		cin >> nums[i];
+	}
+
+	//reversing the array.
+	reverse(nums, ARR_LEN);
+
+	//returning the reversed array.
+	return nums;
 }
--- HW_week1_OhadShaked/HW_week1_OhadShaked/Utils.h
+++ HW_week1_OhadShaked/HW_week1_OhadShaked/Utils.h
@@ -2,6 +2,6 @@
 #define UTILS_H
 
 void reverse(int* nums, unsigned int size); // Reverses the numbers stored in a given array using the stack structure.
-int* reverse10();
+int* reverse10(); // Takes input for 10 numbers in a array, reverses the array, and returns the reversed array.
 
 #endif // UTILS_H
